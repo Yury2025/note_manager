701548f81f2395a5660d75b73bbb0797357a6997
Yury2025 <byv-work@rambler.ru>
2025-01-20 10:46:25 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Yury'
+title = 'Часть 1. Этап 1. Задание 1'
+content = 'Работа с переменными'
+status = 'В работе'
+created_date = '20-01-2025'
+issue_date = '30-01-2025'
+
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания заметки: ', created_date)
+print('Дата истечения заметки (дедлайн): ', issue_date)

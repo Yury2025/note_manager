ed733c372e1e81ce37cad3dda5f35f940844e034
Yury2025 <byv-work@rambler.ru>
2025-01-21 17:29:27 +0400

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,13 @@
+username = 'Yury'
+title = 'Часть 1. Этап 1. Задание 1'
+content = 'Работа с переменными'
+status = 'В работе'
 created_date = '20-01-2025'
 issue_date = '30-01-2025'
 
-print('Дата создания заметки: ', created_date[0:-5])
-print('Дата истечения заметки (дедлайн): ', issue_date[0:-5])
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания заметки: ', created_date[:-5])
+print('Дата истечения заметки (дедлайн): ', issue_date[:-5])
